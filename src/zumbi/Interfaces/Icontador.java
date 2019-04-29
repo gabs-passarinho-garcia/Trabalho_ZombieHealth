8eedca8705cd6c445e34bf467e3bd952a5351814
Gabriel de Freitas <gabrielfreitasgarcia@globo.com>
2019-04-29 20:25:40 -0300

    criei interface contadora

---
+++ src/zumbi/Interfaces/Icontador.java
@@ -0,0 +1,8 @@
+package zumbi.Interfaces;
+
+public interface Icontador {
+	public static int conta_porcentagem(String[][] sintomas) {
+		//Método que analisa uma matriz de sintomas e pode devolver qual o sintoma mais presente, o que está presente em metade dos casos e etc.
+	}
+	
+}
