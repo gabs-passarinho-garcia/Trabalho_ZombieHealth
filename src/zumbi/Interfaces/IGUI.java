c4115d1ca31ed796c2e8219be71a09ba73c66673
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2019-04-30 00:32:32 -0300

    Adicionada Interface IGUI

---
+++ src/zumbi/Interfaces/IGUI.java
@@ -0,0 +1,57 @@
+public interface IGUI{
+	
+	public void initGUI();
+	public void connectPaciente(Pantient enfermo);
+	public void connectDoutor(Doctor doutor);
+
+}
+
+/*
+ * IGUI
+ * Deve ser instanciado para funcionar
+ * ----------------------------------------------------------------------------
+ * 
+ * public void initGUI()
+ * 
+ * Argumentos:
+ * 		Nenhum
+ * 
+ * Retorno:
+ * 		Nenhum
+ * 
+ * Comportamento:
+ * 		Cria uma Graphical User Interface, mostrando uma ilustração do Doutor
+ * 		e do Paciente, além de um botão que permite o Doutor fazer o
+ * 		diagnostico do Paciente.
+ * 
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * public void connectPaciente(Pantient enfermo);
+ * 
+ * Argumentos:
+ * 		Patient enfermo: paciente a ser diagnosticado.
+ * 
+ * Retorno:
+ * 		Nenhum
+ * 
+ * Comportamento:
+ * 		Conecta o paciente à interface gráfica.
+ * 
+ * 
+ * ----------------------------------------------------------------------------
+ *
+ * 
+ * public void connectDoutor(Doctor doutor);
+ * 
+ * Argumentos:
+ * 		Doctor doutor: doutor à diagnosticar o paciente.
+ * 
+ * Retorno:
+ * 		Nenhum.
+ * 
+ * Comportamento:
+ * 		Conecta o doutor à interface gráfica.
+ * 
+ * 
+ * ----------------------------------------------------------------------------
