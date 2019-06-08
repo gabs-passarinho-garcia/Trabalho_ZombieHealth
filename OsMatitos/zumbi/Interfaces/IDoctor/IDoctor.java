560792d0136b6f4fb3b3d16bbcfaf87a3a6f7038
igorknz <47696642+igorknz@users.noreply.github.com>
2019-06-08 15:20:31 -0300

    Add files via upload

---
+++ OsMatitos/zumbi/Interfaces/IDoctor/IDoctor.java
@@ -0,0 +1,7 @@
+package zumbi.Interfaces.IDoctor;
+
+import zumbi.Interfaces.IPatient.ITableProducerReceptacle;
+
+public interface IDoctor extends IEnquirer, IResponderReceptacle, ITableProducerReceptacle{
+
+}
