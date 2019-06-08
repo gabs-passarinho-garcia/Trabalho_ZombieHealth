f7e65649373585bcf2093932a1e67d0316dc3ddb
ChrisKonishi <chris_angel_2000@live.com>
2019-06-07 23:18:03 -0300

    Mesma coisa do anterior

---
+++ OsMatitos/zumbi/Classes/ContaSuperFactory/ContaSuperFactory.java
@@ -0,0 +1,10 @@
+package osMatitos.Classes.ContaSuperFactory;
+
+import zumbi.Interfaces.IContaFactory.*;
+import zumbi.Classes.MelhorFactory.*;
+
+public class ContaSuperFactory {
+	public static IContaFactory criaFabrica() {
+		return new MelhorFactory();
+	}
+}
