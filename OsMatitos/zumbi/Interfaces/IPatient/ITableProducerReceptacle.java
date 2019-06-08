00683e0624377ad94687e792de0112648fc991ed
igorknz <47696642+igorknz@users.noreply.github.com>
2019-06-08 15:16:14 -0300

    Add files via upload

---
+++ OsMatitos/zumbi/Interfaces/IPatient/ITableProducerReceptacle.java
@@ -0,0 +1,7 @@
+package zumbi.Interfaces.IPatient;
+
+import zumbi.Interfaces.IDataSet.ITableProducer;
+
+public interface ITableProducerReceptacle {
+	public void connect(ITableProducer producer);
+}
