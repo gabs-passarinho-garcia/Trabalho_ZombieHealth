6766a08868ed566ab580962f45b68ade90f24a7f
NakaiSan <38078218+NakaiSan@users.noreply.github.com>
2019-06-08 17:37:22 -0300

    Add files via upload

---
+++ OsMatitos/zumbi/Componentes/RedutorPossibilidades/FabricaRedutor.java
@@ -0,0 +1,7 @@
+package zumbi.Componentes.RedutorPossibilidades;
+
+public class FabricaRedutor {
+	public static RedutorPossibilidades criarRedutor() {
+		return new RedutorPossibilidades();
+	}
+}
